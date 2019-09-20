9fd6174332247642b69a4ee22ce62f06519aa639
root <root@localhost.localdomain>
2019-09-20 16:58:45 -0400

    modified:   Commands/colors.sh 	modified:   Commands/kali_update 	deleted:    Commands/Update_Results/results.log

---
+++ Commands/colors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Usage
+#Import this script into another one: source /file/location/colors.sh
+#Use colors in different script after import: ${RED}
+
+#Primary Colors
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BROWN='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+
+#Secondary Colors
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+
+#No color
+NO_COLOR='\033[0m'
+
